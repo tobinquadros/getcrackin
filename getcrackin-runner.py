f04a4ac929f99dc25745364e6ddb931865dcb89f
Tobin Quadros <tobinquadros@gmail.com>
2015-01-06 03:07:55 -0600

    Try travis_wait command for timeout.

--- getcrackin-runner.py
+++ getcrackin-runner.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 """Convenience wrapper for running getcrackin directly from source tree."""
 
-
 from getcrackin.getcrackin import main
-
 
 if __name__ == '__main__':
     main()
--- tests/getcrackin_test.py
+++ tests/getcrackin_test.py
@@ -4,17 +4,27 @@
 import subprocess
 import unittest
 
+def shell_out(cmdline):
+    proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = proc.communicate()[0].rstrip()
+    print "actual is {}".format(output)
+    return output
+
 class ParserTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_no_options(self):
+        cmdline = ["./getcrackin-runner.py"]
+        actual = shell_out(cmdline)
+        expected = ""
+        self.assertEqual(actual, expected, "Should have default output.")
+
     def test_version_option(self):
         cmdline = ["./getcrackin-runner.py", "--version"]
-        proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        actual = proc.communicate()
-        print "actual is {}".format(actual[0].rstrip())
+        actual = shell_out(cmdline)
         expected = "0.0.1"
-        self.assertEqual(actual[0].rstrip(), expected, "Version number doesn't match.")
+        self.assertEqual(actual, expected, "Version number doesn't match.")
 
 if __name__ == "__main__":
     unittest.main()
